7a42f61ee4a5918e98a102b029416785c453f42b
yasyasench <yasyasench@gmail.com>
2024-12-19 11:42:21 +0100

    add constant

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,11 @@
 // src/controllers/auth.js
 
-import { loginUser, logoutUser, refreshUsersSession, registerUser } from '../services/auth.js';
+import {
+    loginUser,
+    logoutUser,
+    refreshUsersSession,
+    registerUser
+} from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -15,24 +20,23 @@ export const registerUserController = async (req, res) => {
 
 
 export const loginUserController = async (req, res) => {
-  await loginUser(req.body);
+	const session = await loginUser(req.body);
 
- res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
+	res.cookie("sessionId", session._id, {
+		httpOnly: true,
+		expires: new Date(Date.now() + THIRTY_DAYS),
+	});
 
-  res.json({
-    status: 200,
-    message: 'Successfully logged in an user!',
-    data: {
-      accessToken: session.accessToken,
-    },
-  });
+	res.cookie("refreshToken", session.refreshToken, {
+		httpOnly: true,
+		expires: new Date(Date.now() + THIRTY_DAYS),
+	});
+
+	res.status(200).json({
+		status: 200,
+		message: "Successfully logged in an user!",
+		data: { accessToken: session.accessToken },
+	});
 };
 
 
