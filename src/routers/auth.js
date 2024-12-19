22883bdfb18e39db40a1957ecbce8341195b3a88
yasyasench <yasyasench@gmail.com>
2024-12-19 11:16:14 +0100

    add import

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,7 +3,7 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
-import { loginUserController, logoutUserController, registerUserController } from '../controllers/auth.js';
+import { loginUserController, logoutUserController, refreshUserSessionController, registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
